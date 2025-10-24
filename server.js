6256c44c813c1fd4ff55bc60aeb598467ef2f4d9
Dyson <Dysonthomas112@gmail.com>
2025-10-24 12:27:30 +0530

    cors updated

--- server.js
+++ server.js
@@ -8,7 +8,16 @@ const authRoute = require("./authRoute");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors(
+  {
+   origin: [
+     'http://localhost:4200',           // Local Angular dev
+    'http://192.168.1.13:4200',        // Your local IP
+    'https://YOUR_USERNAME.github.io'  // GitHub Pages (add this later)
+  ],
+  credentials:true
+  }
+));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
